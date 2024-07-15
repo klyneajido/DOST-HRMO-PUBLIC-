d824e495ffb49e3e580e13526a661e5c91703ce3
AndriqKlyne <andriqklyne.ajido@lorma.edu>
2024-07-15 11:21:04 +0800

    changed money format

--- apply_page.php
+++ apply_page.php
@@ -33,7 +33,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $contact_number = htmlspecialchars($_POST['contact_number']);
 
     // File upload handling function
-    function uploadFile($file, $upload_dir) {
+    function uploadFile($file, $upload_dir)
+    {
         if ($file['error'] === UPLOAD_ERR_OK) {
             $tmp_name = $file['tmp_name'];
             $file_name = basename($file['name']);
@@ -94,6 +95,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
@@ -107,6 +109,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/0dcd39d035.js" crossorigin="anonymous"></script>
 </head>
+
 <body class="scrollbar" id="style-5">
     <div class="force-overflow">
         <!-- Navbar -->
@@ -120,19 +123,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <h2 class="mb-4">Apply for <?php echo htmlspecialchars($job_title); ?></h2>
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?job_id=" . $job_id); ?>" enctype="multipart/form-data">
                     <div class="mb-3">
-                        <label for="lastname" class="form-label">Lastname *</label>
+                        <label for="lastname" class="form-label">Lastname<span class="red-asterisk">*</span></label>
                         <input type="text" class="form-control" name="lastname" id="lastname" required>
                     </div>
                     <div class="mb-3">
-                        <label for="firstname" class="form-label">Firstname *</label>
+                        <label for="firstname" class="form-label">Firstname <span class="red-asterisk">*</span></label>
                         <input type="text" class="form-control" name="firstname" id="firstname" required>
                     </div>
                     <div class="mb-3">
-                        <label for="middlename" class="form-label">Middlename *</label>
+                        <label for="middlename" class="form-label">Middlename <span class="red-asterisk">*</span></label>
                         <input type="text" class="form-control" name="middlename" id="middlename" required>
                     </div>
                     <div class="mb-3">
-                        <label class="form-label">Sex *</label>
+                        <label class="form-label">Sex <span class="red-asterisk">*</span></label>
                         <div>
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="sex" id="male" value="male" required>
@@ -145,23 +148,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </div>
                     </div>
                     <div class="mb-3">
-                        <label for="address" class="form-label">Address *</label>
+                        <label for="address" class="form-label">Address <span class="red-asterisk">*</span></label>
                         <input type="text" class="form-control" name="address" id="address" required>
                     </div>
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email Address *</label>
+                        <label for="email" class="form-label">Email Address <span class="red-asterisk">*</span></label>
                         <input type="email" class="form-control" name="email" id="email" required>
                     </div>
                     <div class="mb-3">
-                        <label for="contact" class="form-label">Contact Number *</label>
+                        <label for="contact" class="form-label">Contact Number  <span class="red-asterisk">*</span></label>
                         <input type="tel" class="form-control" name="contact_number" id="contact" required>
                     </div>
                     <div class="mb-3">
-                        <label for="application_letter" class="form-label">Application Letter *</label>
+                        <label for="application_letter" class="form-label">Application Letter  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="application_letter" id="application_letter" required>
                     </div>
                     <div class="mb-3">
-                        <label for="pds" class="form-label">Fully accomplished Personal Data Sheet with recent passport-sized picture *</label>
+                        <label for="pds" class="form-label">Fully accomplished Personal Data Sheet with recent passport-sized picture  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="pds" id="pds" required>
                     </div>
                     <div class="mb-3">
@@ -169,19 +172,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <input type="file" class="form-control" name="performance_rating" id="performance_rating">
                     </div>
                     <div class="mb-3">
-                        <label for="certificate_eligibility" class="form-label">Photocopy of certificate of eligibility/rating/license *</label>
+                        <label for="certificate_eligibility" class="form-label">Photocopy of certificate of eligibility/rating/license  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="certificate_eligibility" id="certificate_eligibility" required>
                     </div>
                     <div class="mb-3">
-                        <label for="transcript_records" class="form-label">Photocopy of Transcript of Records *</label>
+                        <label for="transcript_records" class="form-label">Photocopy of Transcript of Records  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="transcript_records" id="transcript_records" required>
                     </div>
                     <div class="mb-3">
-                        <label for="certificate_employment" class="form-label">Photocopy of Certificate of Employment/s *</label>
+                        <label for="certificate_employment" class="form-label">Photocopy of Certificate of Employment/s  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="certificate_employment" id="certificate_employment" required>
                     </div>
                     <div class="mb-3">
-                        <label for="trainings_seminars" class="form-label">Proof of trainings and seminars attended *</label>
+                        <label for="trainings_seminars" class="form-label">Proof of trainings and seminars attended  <span class="red-asterisk">*</span></label>
                         <input type="file" class="form-control" name="trainings_seminars" id="trainings_seminars" required>
                     </div>
                     <div class="mb-3">
@@ -191,8 +194,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <button type="submit" class="btn btn-primary">Submit Application</button>
                 </form>
             </div>
+
         </section>
+        <div class="col-md-6 mt-3">
+            <!-- Empty column to create space -->
+        </div>
         <?php include("footer.php") ?>
     </div>
 </body>
+
 </html>
